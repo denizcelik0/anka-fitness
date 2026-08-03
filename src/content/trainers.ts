beda3561500ada77bd78d23a9bedd246334d7e12
deniz267 <deniz2006267@gmail.com>
2026-08-03 19:54:05 +0300

    fix: WhatsApp yonlendirme numarasi guncellendi ve Efe antrenor siralamasi degistirildi

--- src/content/trainers.ts
+++ src/content/trainers.ts
@@ -9,8 +9,8 @@ export type Trainer = {
 export const trainers: Trainer[] = [
   { id: "t1", name: "Murat", image: encodeURI("/images/trainers/Murat.jpeg") },
   { id: "t2", name: "Gamze", image: encodeURI("/images/trainers/Gamze.jpeg") },
-  { id: "t3", name: "Efe", image: encodeURI("/images/trainers/Efe.jpeg") },
-  { id: "t4", name: "Efe", image: encodeURI("/images/trainers/Efe2.jpeg") },
+  { id: "t3", name: "Efe", image: encodeURI("/images/trainers/Efe2.jpeg") },
+  { id: "t4", name: "Efe", image: encodeURI("/images/trainers/Efe.jpeg") },
   { id: "t5", name: "Ceren", image: encodeURI("/images/trainers/Ceren.jpeg") },
   { id: "t6", name: "Nisa", image: encodeURI("/images/trainers/Nisa.jpeg") },
   { id: "t7", name: "Emre", image: encodeURI("/images/trainers/Emre.jpeg") },
